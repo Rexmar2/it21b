ac5e69e39ee71aac6cd8656d4f7566512d07d5af
Rexmar2 <hazerulincuna@gmail.com>
2025-09-16 08:18:02 +0800

    Added Warrior class as inherited from Hero class

--- hero.js
+++ hero.js
@@ -13,16 +13,22 @@ class Hero{
         return this.#health
     }
 showStats(){
-    console.log("Name: "+this.name);
-console.log("Health: "+this.#health);
-console.log("Attack: "+this.attack);
-console.log("------------------");
+     console.log("Name: "+this.name);
+     onsole.log("Health: "+this.#health);
+     console.log("Attack: "+this.attack);
+     console.log("------------------");
+  }
 }
+class Warrior extends Hero {
+ useAbility(){
+    console.log('${this.name} user Power Strike');
+ }
+     
 }
-const hero1 = new Hero("Thorin",100,10);
+
+const Thorin = new Warrior("Thorin",100,10);
 const hero2 = new Hero("Gandalf",100,8);
 
-hero1.showStats();
-hero2.showStats();
+Thorin.useAbility();
 
 
